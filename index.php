6f8121bc32e4a5bb58a052c04ce73cfd7af2968f
Swapan267 <105355427+Swapan267@users.noreply.github.com>
2022-05-11 16:00:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <?php
     // Using two forward slashes we create comments in php.
     // Within the php tag, we can insert values into our HTML.
-    echo "<h1>Welcome!</h1>";
+    echo "<h1>Welcome SWAPAN PRIYA!</h1>";
     ?>
   </body>
 </html>
